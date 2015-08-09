2fb334ed0446adb4bea5c17859aab21fb1a696bd
David Baynard <davidbaynard@gmail.com>
2015-08-09 21:55:38 +0100

    Start test suite

---
+++ test/TestSuite.hs
@@ -0,0 +1,15 @@
+{-|
+Module      : TestSuite
+Description : Test module for Ucam-Webauth
+Maintainer  : David Baynard <davidbaynard@gmail.com>
+
+-}
+
+module TestSuite (
+    module TestSuite
+  , module X
+)   where
+
+import ClassyPrelude as X
+import UcamWebauth as X
+import RavenAuth as X
