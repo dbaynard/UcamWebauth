7d300da2af01e19e587d2671afac7a9d60557c71
David Baynard <davidbaynard@gmail.com>
2015-08-12 02:06:03 +0100

    Move protocol Internal file from Wai directory

---
+++ src/Network/Protocol/UcamWebauth/Internal.hs
@@ -0,0 +1,11 @@
+{-|
+Module      : Network.Protocol.UcamWebauth.Internal
+Description : Ucam-Webauth protocol internals
+Maintainer  : David Baynard <davidbaynard@gmail.com>
+
+-}
+
+module Network.Protocol.UcamWebauth.Internal (
+    module Network.Protocol.UcamWebauth.Internal
+)   where
+
