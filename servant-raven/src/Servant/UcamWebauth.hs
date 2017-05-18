965b6681a56e21dc4ed1432db226efb4748d7dda
David Baynard <davidbaynard@gmail.com>
2017-05-18 01:36:35 +0100

    Refactor Servant.UcamWebAuth and document

--- servant-raven/src/Servant/UcamWebauth.hs
+++ servant-raven/src/Servant/UcamWebauth.hs
@@ -19,6 +19,7 @@ for 'readRSAKeyFile'.
 
 -}
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
@@ -44,7 +45,6 @@ import "Ucam-Webauth" Network.Protocol.UcamWebauth as X
 import "base" GHC.Generics
 import "base" Control.Monad.IO.Class
 import "base" Data.Kind
-import "base" Control.Monad
 
 import "errors" Control.Error
 
@@ -58,7 +58,77 @@ import "servant-auth-server" Servant.Auth.Server.SetCookieOrphan ()
 import "jose" Crypto.JOSE.JWK (JWK)
 
 import "aeson" Data.Aeson.Types hiding ((.=))
--- Map structures
+
+------------------------------------------------------------------------------
+--
+-- * Top level functions
+
+-- | Base 64 (URL) encoded 'ByteString's should be serializable as 'OctetStream's.
+-- They are already serializable as 'JSON' thanks to the ToJson instance.
+instance MimeRender OctetStream Base64UBSL where
+    mimeRender _ = unB64UL
+
+-- TODO Make safe
+instance MimeUnrender OctetStream Base64UBSL where
+    mimeUnrender _ = pure . B64UL
+
+-- | UcamWebauthInfo can be converted directly to a JWT.
+instance ToJSON a => ToJWT (UcamWebauthInfo a)
+-- | UcamWebauthInfo can be converted directly from a JWT.
+instance FromJSON a => FromJWT (UcamWebauthInfo a)
+
+-- | Wrap the provided handler function with authentication.
+authenticated
+    :: ThrowAll (Handler protected)
+    => (a -> Handler protected)
+    -> AuthResult a
+    -> Handler protected
+authenticated f (Authenticated user) = f user
+authenticated _ _ = throwAll err401
+
+-- | A bifunctional endpoint for authentication, which both delegates and
+-- responds to the Web Login Service (WLS).
+type UcamWebAuthenticate a
+    = QueryParam "WLS-Response" (SignedAuthResponse 'MaybeValid a) :> Get '[JSON] (UcamWebauthInfo a)
+
+-- | If a GET request is made with no query parameters, redirect (303) to the authentication server.
+--
+-- If a GET request is made with the WLS-Response query parameter, try to
+-- parse that parameter to a 'UcamWebauthInfo a', and then return that
+-- parameter or throw a 401 error.
+ucamWebAuthenticate
+    :: forall a. ToJSON a
+    => SetWAA a
+    -> Maybe (SignedAuthResponse 'MaybeValid a)
+    -> Handler (UcamWebauthInfo a)
+ucamWebAuthenticate settings mresponse = do
+        response <- Handler . needToAuthenticate . liftMaybe $ mresponse
+        Handler . authError . authInfo settings $ response
+    where
+        needToAuthenticate = noteT err303 {errHeaders = [ucamWebauthQuery settings]}
+        authError = withExceptT . const $ err401 { errBody = "Authentication error" }
+
+-- | A bifunctional endpoint for authentication, which both delegates and
+-- responds to the Web Login Service (WLS).
+type UcamWebAuthToken token a
+    = QueryParam "WLS-Response" (SignedAuthResponse 'MaybeValid a) :> Get '[OctetStream] token
+
+-- | Here, if a GET request is made with a valid WLS-Response query parameter, return the
+-- 'UcamWebauthInfo a' as a log in token.
+ucamWebAuthToken
+    :: forall a. ToJSON a
+    => SetWAA a
+    -> Maybe UTCTime
+    -> JWK
+    -> Maybe (SignedAuthResponse 'MaybeValid a)
+    -> Handler Base64UBSL
+ucamWebAuthToken settings mexpires ky mresponse = let jwtCfg = defaultJWTSettings ky in do
+        uwi <- ucamWebAuthenticate settings mresponse
+        Handler . bimapExceptT trans B64UL . ExceptT $ makeJWT uwi jwtCfg mexpires
+    where
+        trans _ = err401 { errBody = "Token error" }
+
+------------------------------------------------------------------------------
 
 newtype User = User Text
     deriving (Eq, Show, Read, Generic)
@@ -68,42 +138,8 @@ instance ToJWT User
 instance FromJSON User
 instance FromJWT User
 
-instance ToJSON a => ToJWT (UcamWebauthInfo a)
-instance FromJSON a => FromJWT (UcamWebauthInfo a)
-
 type Protected
     = "user" :> Get '[JSON] Text
-
-instance MimeRender OctetStream Base64UBSL where
-    mimeRender _ = unB64UL
-instance MimeUnrender OctetStream Base64UBSL where
-    mimeUnrender _ = pure . B64UL
-
-protected :: ThrowAll (Handler protected) => (a -> Handler protected) -> AuthResult a -> Handler protected
-protected f (Authenticated user) = f user
-protected _ _ = throwAll err401
-
-type UcamWebAuthenticate route a
-    = route :> QueryParam "WLS-Response" (SignedAuthResponse 'MaybeValid a) :> Get '[JSON] (UcamWebauthInfo a)
-
-type UcamWebAuthToken route token a
-    = route :> QueryParam "WLS-Response" (SignedAuthResponse 'MaybeValid a) :> Get '[OctetStream] token
-
-{-ucamWebAuthenticate :: forall r a . ToJSON a => SetWAA a -> Server (Raven r a)-}
-ucamWebAuthenticate :: forall a. ToJSON a => SetWAA a -> Maybe (SignedAuthResponse 'MaybeValid a) -> Handler (UcamWebauthInfo a)
-ucamWebAuthenticate settings mresponse = do
-        response <- Handler . needToAuthenticate . liftMaybe $ mresponse
-        Handler . ravenError . authInfo settings $ response
-    where
-        needToAuthenticate = noteT err303 {errHeaders = [ucamWebauthQuery settings]}
-        ravenError = withExceptT . const $ err401 { errBody = "Raven error" }
-
-ucamWebAuthToken :: forall a. ToJSON a => SetWAA a -> Maybe UTCTime -> JWK -> Maybe (SignedAuthResponse 'MaybeValid a) -> Handler Base64UBSL
-ucamWebAuthToken settings mexpires ky mresponse = let jwtCfg = defaultJWTSettings ky in do
-        uwi <- ucamWebAuthenticate settings mresponse
-        Handler . bimapExceptT trans B64UL . ExceptT $ makeJWT uwi jwtCfg mexpires
-    where
-        trans _ = err401 { errBody = "Token error" }
 
 type Unprotected
     = "login" :> ReqBody '[JSON] (UcamWebauthInfo Text) :> PostNoContent '[JSON]
@@ -119,12 +155,12 @@ unprotected cs jwts = checkCreds cs jwts :<|> serveDirectoryFileServer "example/
 
 type API auths a
     = Auth auths User :> Protected
-    :<|> UcamWebAuthToken "authenticate" Base64UBSL a
+    :<|> "authenticate" :> UcamWebAuthToken Base64UBSL a
     :<|> Unprotected
 
 server :: ToJSON a => SetWAA a -> CookieSettings -> JWTSettings -> JWK -> Server (API auths a)
 server rs cs jwts ky =
-        protected (return . (\(User user) -> user))
+        authenticated (return . (\(User user) -> user))
     :<|> ucamWebAuthToken rs Nothing ky
     :<|> unprotected cs jwts
 
@@ -170,6 +206,3 @@ checkCreds cookieSettings jwtSettings _ = do
         Nothing           -> throwError err401
         Just applyCookies -> return $ applyCookies NoContent
 
-------------------------------------------------------------------------------
---
--- * Top level functions
