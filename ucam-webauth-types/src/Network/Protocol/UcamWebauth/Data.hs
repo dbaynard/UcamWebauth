0cb09c16369f49e04eb2f5faa5a2e1172d062b3c
David Baynard <davidbaynard@gmail.com>
2017-06-01 01:14:33 +0100

    Split data definitions into internal and Data modules

--- ucam-webauth-types/src/Network/Protocol/UcamWebauth/Data.hs
+++ ucam-webauth-types/src/Network/Protocol/UcamWebauth/Data.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_HADDOCK hide, not_here #-}
 {-# LANGUAGE PackageImports #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE RecordWildCards #-}
@@ -28,6 +27,7 @@ module Network.Protocol.UcamWebauth.Data
   , approveLife
   , approveParams
 
+  -- $request
   , AuthRequest()
   , ucamQVer
   , ucamQUrl
@@ -40,16 +40,12 @@ module Network.Protocol.UcamWebauth.Data
   , ucamQFail
 
   , SignedAuthResponse()
-  , makeSignedAuthResponse
   , ucamAResponse
   , ucamAToSign
   , ucamAKid
   , ucamASig
 
-  , IsValid(..)
-
   , AuthResponse()
-  , makeAuthResponse
   , ucamAVer
   , ucamAStatus
   , ucamAMsg
@@ -132,16 +128,10 @@ module Network.Protocol.UcamWebauth.Data
   , decodeASCII'
   ) where
 
+import Network.Protocol.UcamWebauth.Data.Internal
+
 -- Prelude
-import "base" Data.Data
-import "base" GHC.Generics
 import "base" Control.Applicative
-import "base" Data.String
-import "base" Data.Semigroup
-import "containers" Data.IntMap (IntMap)
-import qualified "containers" Data.IntMap as IntMap
-import "errors" Control.Error
-import "base" Control.Arrow ((&&&))
 
 -- Settings
 import "microlens" Lens.Micro
@@ -155,42 +145,18 @@ import qualified "bytestring" Data.ByteString.Char8 as B
 import qualified "bytestring" Data.ByteString.Lazy.Char8 as BL
 import qualified "bytestring" Data.ByteString.Lazy as BSL
 import "text" Data.Text (Text)
-import "text" Data.Text.Encoding
 import qualified "text" Data.Text as T
-import qualified "text" Data.Text.Lazy.Encoding as TL
 import "base" Data.Char (isAlphaNum, isAscii)
-
-import "aeson" Data.Aeson.Types
 
 -- Time
 import "timerep" Data.Time.RFC3339
 import "time" Data.Time
 
--- HTTP protocol
-import "http-types" Network.HTTP.Types
+------------------------------------------------------------------------------
+-- * Lenses
 
 ------------------------------------------------------------------------------
--- * Core data types and associated functions
-
-------------------------------------------------------------------------------
--- ** Return type
-
-{-|
-  'UcamWebauthInfo' is returned from this module. The parameter 'a' represents data sent
-  in the initial connection, that must be returned. The constructor and accessors are *not*
-  exported from the module, to present an abstract API.
--}
-data UcamWebauthInfo a = AuthInfo
-    { _approveUniq :: (UTCTime, Text)
-    , _approveUser :: Text
-    , _approveAttribs :: [Ptag]
-    , _approveLife :: Maybe TimePeriod
-    , _approveParams :: Maybe a
-    } deriving (Show, Eq, Ord, Generic, Generic1, Typeable, Data)
-
-instance ToJSON a => ToJSON (UcamWebauthInfo a)
-instance FromJSON a => FromJSON (UcamWebauthInfo a)
-
+-- ** 'UcamWebauthInfo'
 
 {-|
   Unique representation of response, composed of issue and id
@@ -223,38 +189,13 @@ approveParams :: UcamWebauthInfo a `Lens'` Maybe a
 approveParams f AuthInfo{..} = (\_approveParams -> AuthInfo{_approveParams, ..}) <$> f _approveParams
 
 ------------------------------------------------------------------------------
--- ** Type Synonyms
-
-{-|
-  A synonym to abstract much behaviour over a generic string type.
--}
-type StringType = ByteString
-
-------------------------------------------------------------------------------
--- ** Request and response
+-- ** 'AuthRequest'
 {- $request
   The handshake between the @WLS@ and @WAA@ are represented using the 'AuthRequest'
   and 'SignedAuthResponse' data types. The 'AuthResponse' type represents the
   content of a 'SignedAuthResponse'. Constructors and accessors are not exported,
   and the 'AuthRequest' should be build using the smart constructors provided.
 -}
-
-{-|
-  An 'AuthRequest' is constructed by the @WAA@, using the constructor functions
-  of this module. The parameter represents data to be returned to the application
-  after authentication.
--}
-data AuthRequest a = MakeAuthRequest
-    { _ucamQVer :: WLSVersion
-    , _ucamQUrl :: Text
-    , _ucamQDesc :: Maybe ASCII
-    , _ucamQAauth :: Maybe [AuthType]
-    , _ucamQIact :: Maybe YesNo
-    , _ucamQMsg :: Maybe Text
-    , _ucamQParams :: Maybe a
-    , _ucamQDate :: Maybe UTCTime
-    , _ucamQFail :: Maybe YesOnly
-    } deriving (Show, Eq, Ord, Generic1, Typeable, Data)
 
 {-|
   The version of @WLS.@ 1, 2 or 3.
@@ -312,28 +253,9 @@ ucamQDate f MakeAuthRequest{..} = (\_ucamQDate -> MakeAuthRequest{_ucamQDate, ..
 ucamQFail :: AuthRequest a `Lens'` Maybe YesOnly
 ucamQFail f MakeAuthRequest{..} = (\_ucamQFail -> MakeAuthRequest{_ucamQFail, ..}) <$> f _ucamQFail
 
-{-|
-  A 'SignedAuthResponse' represents the data returned by the @WLS@, including a
-  representation of the content returned (in the 'AuthResponse' data type), and
-  the cryptographic signature, for verification.
-
-  The phantom parameter 'valid' corr
--}
-data SignedAuthResponse (valid :: IsValid) a = SignedAuthResponse
-    { _ucamAResponse :: AuthResponse a
-    , _ucamAToSign :: ByteString
-    , _ucamAKid :: Maybe KeyID
-    , _ucamASig :: Maybe UcamBase64BS
-    } deriving (Show, Eq, Ord, Generic, Generic1, Typeable, Data)
-
-makeSignedAuthResponse
-    :: AuthResponse a
-    -> SignedAuthResponse 'MaybeValid a
-makeSignedAuthResponse _ucamAResponse = SignedAuthResponse{..}
-    where
-        _ucamAToSign = mempty
-        _ucamAKid = empty
-        _ucamASig = empty
+--------------------------------------------------
+-- ** 'SignedAuthResponse'
+--------------------------------------------------
 
 {-|
   The bit of the response that is signed
@@ -359,53 +281,9 @@ ucamAKid f SignedAuthResponse{..} = (\_ucamAKid -> SignedAuthResponse{_ucamAKid,
 ucamASig :: SignedAuthResponse valid a `Lens'` Maybe UcamBase64BS
 ucamASig f SignedAuthResponse{..} = (\_ucamASig -> SignedAuthResponse{_ucamASig, ..}) <$> f _ucamASig
 
-{-|
-  The intended use of this is with 'IsValid' as a kind (requires the 'DataKinds' extension).
-  The data constructors 'Valid' and 'MaybeValid' are now type constructors, which indicate the
-  validity of a 'SignedAuthResponse'.
-
-  TODO This is not exported?
--}
-data IsValid
-    = MaybeValid
-    | Valid
-    deriving (Show, Read, Eq, Ord, Enum, Bounded, Generic, Typeable, Data)
-
-{-|
-  An 'AuthResponse' represents the content returned by the @WLS@. The validation
-  machinery in this module returns the required data as a 'UcamWebauthInfo' value.
--}
-data AuthResponse a = AuthResponse
-    { _ucamAVer :: WLSVersion
-    , _ucamAStatus :: StatusCode
-    , _ucamAMsg :: Maybe Text
-    , _ucamAIssue :: UTCTime
-    , _ucamAId :: Text
-    , _ucamAUrl :: Text
-    , _ucamAPrincipal :: Maybe Text
-    , _ucamAPtags :: Maybe [Ptag]
-    , _ucamAAuth :: Maybe AuthType
-    , _ucamASso :: Maybe [AuthType]
-    , _ucamALife :: Maybe TimePeriod
-    , _ucamAParams :: Maybe a
-    } deriving (Show, Eq, Ord, Generic, Generic1, Typeable, Data)
-
-makeAuthResponse
-    :: WLSVersion
-    -> UTCTime
-    -> AuthResponse a
-makeAuthResponse _ucamAVer _ucamAIssue = AuthResponse{..}
-    where
-        _ucamAStatus = BadRequest400
-        _ucamAMsg = mempty
-        _ucamAId = mempty
-        _ucamAUrl = mempty
-        _ucamAPrincipal = mempty
-        _ucamAPtags = empty
-        _ucamAAuth = empty
-        _ucamASso = empty
-        _ucamALife = empty
-        _ucamAParams = empty
+--------------------------------------------------
+-- ** 'AuthResponse'
+--------------------------------------------------
 
 {-|
   The version of @WLS@: 1, 2 or 3
@@ -485,21 +363,6 @@ ucamAParams f AuthResponse{..} = (\_ucamAParams -> AuthResponse{_ucamAParams, ..
 getAuthInfo :: Alternative f => SignedAuthResponse 'Valid a -> f (UcamWebauthInfo a)
 getAuthInfo = extractAuthInfo . _ucamAResponse
 
-{-|
-  Convert an 'AuthResponse' into a 'UcamWebauthInfo' for export.
-
-  This should not be exported. Instead export 'getAuthInfo'
--}
-extractAuthInfo :: Alternative f => AuthResponse a -> f (UcamWebauthInfo a)
-extractAuthInfo AuthResponse{..} = maybe empty pure $ do
-        _approveUser <- _ucamAPrincipal
-        return AuthInfo{..}
-        where
-            _approveUniq = (_ucamAIssue, _ucamAId)
-            _approveAttribs = _ucamAPtags ?: empty
-            _approveLife = _ucamALife
-            _approveParams = _ucamAParams
-
 ------------------------------------------------------------------------------
 -- * Typed representations of protocol data
 {- $typed
@@ -508,217 +371,7 @@ extractAuthInfo AuthResponse{..} = maybe empty pure $ do
 -}
 
 ------------------------------------------------------------------------------
--- *** Protocol version
-
-{-|
-  Intended to be used as values, but Kind promotion means they can be used as types.
--}
-data WLSVersion
-    = WLS1 -- ^ Version 1 of the protocol. In the Raven implementation, failures use this version
-    | WLS2 -- ^ Version 2
-    | WLS3 -- ^ Version 3. Used for successful reponses by the Raven implementation
-    deriving (Read, Eq, Ord, Enum, Bounded, Generic, Typeable, Data)
-
-instance Show WLSVersion where
-    show = displayWLSVersion
-
-{-|
-  Used for 'Show' instance.
--}
-displayWLSVersion :: IsString a => WLSVersion -> a
-displayWLSVersion WLS1 = "1"
-displayWLSVersion WLS2 = "2"
-displayWLSVersion WLS3 = "3"
-
-{-|
-  Like the 'Show' instance, but typed to 'StringType'.
-
-  -- TODO Rename to bsWLSVersion
--}
-textWLSVersion :: WLSVersion -> StringType
-textWLSVersion = displayWLSVersion
-
-------------------------------------------------------------------------------
--- *** Authentication types available
-
-{-|
-  An enumeration of valid authentication types. The protocol currently only defines one
-  valid type.
--}
-data AuthType
-    = Pwd -- ^ pwd: Username and password
-    deriving (Read, Eq, Ord, Enum, Bounded, Generic, Typeable, Data)
-
-instance Show AuthType where
-    show = displayAuthType
-
-{-|
-  Implement show generically
--}
-displayAuthType :: IsString a => AuthType -> a
-displayAuthType Pwd = "pwd"
-
-------------------------------------------------------------------------------
--- *** Data possibly useful for authorization (ptags)
-
-{-|
-  This is only in protocol versions ≥ 3
--}
-data Ptag
-    = Current -- ^ User is current member of university
-    deriving (Read, Eq, Ord, Enum, Bounded, Generic, Typeable, Data)
-
-instance Show Ptag where
-    show = displayPtag
-
-{-|
-  Generic 'Show' implementation
--}
-displayPtag :: IsString a => Ptag -> a
-displayPtag Current = "current"
-
-instance ToJSON Ptag
-instance FromJSON Ptag
-
-------------------------------------------------------------------------------
--- *** HTTP response codes
-{- $statusCodes
-  A data type representing the HTTP status codes in the protocol. This is compatible
-  with the 'Status' type, but using the algebraic data type makes working with it
-  a little nicer.
--}
-
-{-|
-  The valid HTTP status codes, according to the protocol.
-
-  'BadRequest400' is present as a default, if there is any other code received.
--}
-data StatusCode
-    = Ok200          -- ^ Authentication successful
-    | Gone410        -- ^ Cancelled by the user
-    | NoAuth510      -- ^ No mutually acceptable authentication types
-    | ProtoErr520    -- ^ Unsupported protocol version (Only for version 1)
-    | ParamErr530    -- ^ General request parameter error
-    | NoInteract540  -- ^ Interaction would be required but has been blocked
-    | UnAuthAgent560 -- ^ Application agent is not authorised
-    | Declined570    -- ^ Authentication declined
-    | BadRequest400  -- ^ Response not covered by any protocol responses
-    deriving (Show, Read, Eq, Ord, Bounded, Generic, Typeable, Data)
-
-instance Enum StatusCode where
-    toEnum = fromMaybe BadRequest400 . flip IntMap.lookup responseCodes
-    fromEnum = statusCode . getStatus
-
-{-|
-  An 'IntMap' of 'Status' code numbers in the protocol to their typed representations.
--}
-responseCodes :: IntMap StatusCode
-responseCodes = IntMap.fromList . fmap (statusCode . getStatus &&& id) $ [Ok200, Gone410, NoAuth510, ProtoErr520, ParamErr530, NoInteract540, UnAuthAgent560, Declined570]
-
-{-|
-  Convert to the 'Status' type, defaulting to 'badRequest400' for a bad request
--}
-getStatus :: StatusCode -> Status
-getStatus Ok200 = ok200
-getStatus Gone410 = gone410
-getStatus NoAuth510 = noAuth510
-getStatus ProtoErr520 = protoErr520
-getStatus ParamErr530 = paramErr530
-getStatus NoInteract540 = noInteract540
-getStatus UnAuthAgent560 = unAuthAgent560
-getStatus Declined570 = declined570
-getStatus _ = badRequest400
-
-------------------------------------------------------------------------------
--- *** 'Status' values
-
-noAuth510, protoErr520, paramErr530, noInteract540, unAuthAgent560, declined570 :: Status
-noAuth510      = mkStatus 510 "No mutually acceptable authentication types"
-protoErr520    = mkStatus 520 "Unsupported protocol version (Only for version 1)"
-paramErr530    = mkStatus 530 "General request parameter error"
-noInteract540  = mkStatus 540 "Interaction would be required but has been blocked"
-unAuthAgent560 = mkStatus 560 "Application agent is not authorised"
-declined570    = mkStatus 570 "Authentication declined"
-
-------------------------------------------------------------------------------
--- *** iact yes or no
-
-{-|
-  This is like a Boolean, but specifically for the ‘iact’ parameter
--}
-data YesNo
-    = No
-    | Yes
-    deriving (Read, Eq, Ord, Enum, Bounded, Generic, Typeable, Data)
-
-instance Show YesNo where
-    show = displayYesNo
-
-displayYesNo :: IsString a => YesNo -> a
-displayYesNo Yes = "yes"
-displayYesNo _ = "no"
-
-{-|
-  Monomorphic variant of 'displayYesNo'
-
-  -- TODO Rename to bsDisplayYesNo
--}
-displayYesNoS :: YesNo -> StringType
-displayYesNoS = displayYesNo
-
-------------------------------------------------------------------------------
--- *** fail yes
-
-{-|
-  Like '()' but specifically for the ‘iact’ parameter
--}
-newtype YesOnly = YesOnly' ()
-    deriving (Read, Eq, Ord, Enum, Bounded, Generic, Typeable, Data)
-
-pattern YesOnly :: YesOnly
-pattern YesOnly = YesOnly' ()
-
-{-# COMPLETE YesOnly #-}
-
-instance Show YesOnly where
-    show = displayYesOnly
-
-displayYesOnly :: IsString a => YesOnly -> a
-displayYesOnly YesOnly = "yes"
-
-{-|
-  Monomorphic variant of 'displayYesOnly'
-
-  -- TODO Rename to bsDisplayYesOnly
--}
-displayYesOnlyS :: YesOnly -> StringType
-displayYesOnlyS = displayYesOnly
-
-------------------------------------------------------------------------------
--- *** Keys
-
-{-|
-  The key id, representing the public key for the @WLS@, is composed of a subset of 'ByteString' identifiers
-
-  Do not export constructors
--}
-newtype KeyID = KeyID { unKeyID :: ByteString }
-    deriving (Read, Eq, Ord, Semigroup, Monoid, IsString, Generic, Typeable, Data)
-
-instance Show KeyID where
-    show = B.unpack . unKeyID
-
-------------------------------------------------------------------------------
 -- *** Time
-
-{-|
-  The modified UTCTime representation used in the protocol, based on RFC 3339. All
-  time zones are 'utc'.
-
-  Do not export constructor or accessor.
--}
-newtype UcamTime = UcamTime { unUcamTime :: Text }
-    deriving (Show, Read, Eq, Ord, Semigroup, Monoid, IsString, Generic, Typeable, Data)
 
 zonedUcamTime :: UcamTime -> Maybe ZonedTime
 zonedUcamTime = parseTimeRFC3339 . unUcamTime
@@ -731,61 +384,14 @@ zonedUcamTime = parseTimeRFC3339 . unUcamTime
 ucamTime :: UTCTime -> UcamTime
 ucamTime = UcamTime . T.filter isAlphaNum . formatTimeRFC3339 . utcToZonedTime utc
 
-{-|
-  'DiffTime' with 'ToJSON' and 'FromJSON' instances.
--}
-newtype TimePeriod = TimePeriod { timePeriod :: DiffTime }
-    deriving (Show, Eq, Ord, Num, Generic, Typeable, Data)
-
-secondsFromTimePeriod :: TimePeriod -> Integer
-secondsFromTimePeriod = (`div` 1e12) . diffTimeToPicoseconds . timePeriod
-
-timePeriodFromSeconds :: Integer -> TimePeriod
-timePeriodFromSeconds = TimePeriod . secondsToDiffTime
-
-instance ToJSON TimePeriod where
-    toJSON = toJSON . secondsFromTimePeriod
-    toEncoding = toEncoding . secondsFromTimePeriod
-instance FromJSON TimePeriod where
-    parseJSON = withObject "Seconds" $ \v -> timePeriodFromSeconds
-        <$> v .: "Seconds"
-
 ------------------------------------------------------------------------------
 -- * 'WAASettings' and lenses
-
-{-|
-  The state involved in authentication. This includes the settings as 'WAASettings' and 
-  the request as 'AuthRequest'.
-
-  Do not export constructors or accessors, only lenses.
--}
-data WAAState a = MakeWAAState
-    { _wSet :: WAASettings
-    , _aReq :: AuthRequest a
-    --, _aSrs :: SignedAuthResponse valid a
-    } deriving (Show, Eq, Ord, Generic, Typeable, Data)
 
 wSet :: WAAState a `Lens'` WAASettings
 wSet f MakeWAAState{..} = (\_wSet -> MakeWAAState{_wSet, ..}) <$> f _wSet
 
 aReq :: WAAState a `Lens'` AuthRequest a
 aReq f MakeWAAState{..} = (\_aReq -> MakeWAAState{_aReq, ..}) <$> f _aReq
-
-{-|
-  The settings for the application.
-
-  Do not export constructors or accessors, only lenses.
-  TODO Make urls type safe
--}
-data WAASettings = MakeWAASettings
-    { _authAccepted :: [AuthType]
-    , _needReauthentication :: Maybe YesNo
-    , _syncTimeOut :: NominalDiffTime
-    , _validKids :: [KeyID]
-    , _recentTime :: UTCTime
-    , _applicationUrl :: Text
-    , _wlsUrl :: Text
-    } deriving (Show, Eq, Ord, Generic, Typeable, Data)
 
 {-|
   Accepted authentication types by the protocol.
@@ -853,46 +459,6 @@ wlsUrl f MakeWAASettings{..} = (\_wlsUrl -> MakeWAASettings{_wlsUrl, ..}) <$> f 
 -- * Text encoding
 
 {-|
-  Ensure Base 64 URL text is not confused with other 'ByteString's
--}
-newtype Base64UBS = B64U { unB64U :: ByteString }
-    deriving (Show, Read, Eq, Ord, Semigroup, Monoid, IsString, Generic, Typeable, Data)
-
-instance FromJSON Base64UBS where
-    parseJSON = withObject "Base 64 URL ByteString" $ \v -> B64U . encodeUtf8
-        <$> v .: "Base 64U ByteString"
-
-instance ToJSON Base64UBS where
-    toJSON = toJSON . decodeUtf8 . unB64U
-    toEncoding = toEncoding . decodeUtf8 . unB64U
-
-newtype Base64UBSL = B64UL { unB64UL :: BSL.ByteString }
-    deriving (Show, Read, Eq, Ord, Semigroup, Monoid, IsString, Generic, Typeable, Data)
-
-instance FromJSON Base64UBSL where
-    parseJSON = withObject "Base 64 URL ByteString" $ \v -> B64UL . TL.encodeUtf8
-        <$> v .: "Base 64U ByteString"
-
-instance ToJSON Base64UBSL where
-    toJSON = toJSON . TL.decodeUtf8 . unB64UL
-    toEncoding = toEncoding . TL.decodeUtf8 . unB64UL
-
-{-|
-  Ensure Base 64 URL text modified to fit the Ucam-Webauth protocol is not confused with other 'ByteString's
--}
-newtype UcamBase64BS = UcamB64 { unUcamB64 :: ByteString }
-    deriving (Show, Read, Eq, Ord, Semigroup, Monoid, IsString, Generic, Typeable, Data)
-
-newtype UcamBase64BSL = UcamB64L { unUcamB64L :: BSL.ByteString }
-    deriving (Show, Read, Eq, Ord, Semigroup, Monoid, IsString, Generic, Typeable, Data)
-
-{-|
-  Ensure ASCII text is not confused with other 'ByteString's
--}
-newtype ASCII = ASCII { unASCII :: Text }
-    deriving (Show, Read, Eq, Ord, Semigroup, Monoid, IsString, Generic, Typeable, Data)
-
-{-|
   Convert to the protocol’s version of base64
 -}
 convertB64Ucam :: Base64UBS -> UcamBase64BS
@@ -925,7 +491,7 @@ camDecodeFilter x = x
 
   TODO It should not be a problem, if operating on validated input, but might be worth testing (low priority).
 -}
-decodeUcamB64 :: UcamBase64BS -> StringType
+decodeUcamB64 :: UcamBase64BS -> ByteString
 decodeUcamB64 = B.decodeLenient . unB64U . convertUcamB64
 
 decodeUcamB64L :: UcamBase64BSL -> BSL.ByteString
@@ -934,7 +500,7 @@ decodeUcamB64L = BL.decodeLenient . unB64UL . convertUcamB64L
 {-|
   Unlike decoding, this is fully pure.
 -}
-encodeUcamB64 :: StringType -> UcamBase64BS
+encodeUcamB64 :: ByteString -> UcamBase64BS
 encodeUcamB64 = convertB64Ucam . B64U . B.encode
 
 encodeUcamB64L :: BSL.ByteString -> UcamBase64BSL
