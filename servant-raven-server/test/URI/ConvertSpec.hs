929ea481a107d60c934ac8450afc237ae2bac4cf
David Baynard <davidbaynard@gmail.com>
2017-06-02 02:26:09 +0100

    Replace properties with instances from upstream test suite

--- servant-raven-server/test/URI/ConvertSpec.hs
+++ servant-raven-server/test/URI/ConvertSpec.hs
@@ -4,6 +4,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ApplicativeDo #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module URI.ConvertSpec (main, spec) where
 
@@ -15,28 +17,69 @@ import "hspec" Test.Hspec.QuickCheck
 import "servant-raven-server" URI.Convert
 
 import "base" Data.Maybe
+import "base" Data.Semigroup
 import "base" Control.Monad
+
+import "bytestring" Data.ByteString (ByteString)
+import qualified "bytestring" Data.ByteString.Char8 as B8
 
 import "generic-arbitrary" Test.QuickCheck.Arbitrary.Generic
 
 import qualified "network-uri" Network.URI as NU
 import qualified "uri-bytestring" URI.ByteString as UB
+import "uri-bytestring" URI.ByteString.QQ
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "URI.Convert" $ do
-        {-
-         -prop "converts properly from network-uri" $
-         -    \subj -> uriByteString subj `shouldSatisfy` isJust
-         -}
-        prop "converts properly from uri-bytestring" $
-            \subj -> networkUri subj `shouldSatisfy` isJust
-        prop "interconverts correctly" $
-            \subj -> Just subj === do
-                uriByteString <=< networkUri $ subj
+
+    {-
+     -describe "URI.Convert" $ do
+     -    prop "converts properly from network-uri" $
+     -        \subj -> uriByteString subj `shouldSatisfy` isJust
+     -    prop "converts properly from uri-bytestring" $
+     -        \subj -> networkUri subj `shouldSatisfy` isJust
+     -    prop "interconverts correctly" $
+     -        \subj -> Just subj === do
+     -            uriByteString <=< networkUri $ subj
+     -}
+
+    describe "networkUri" $ (`mapM_` sampleURIs) $ \x ->
+        let disp = B8.unpack . UB.serializeURIRef' $ x in do
+            it ("should convert " <> disp <> " from uri-bytestring")
+            . (`shouldSatisfy` isJust) . networkUri
+            $ x
+
+    describe "interconverts" $ (`mapM_` sampleURIs) $ \x ->
+        let disp = B8.unpack . UB.serializeURIRef' $ x in do
+            it ("should interconvert " <> disp)
+            . (Just x ===)
+            $ do
+                uriByteString <=< networkUri $ x
+
+
+sampleURIs =
+    [ [uri|http://www.example.org/|]
+    , [uri|http://www.example.org?foo=bar&|]
+    , [uri|http://www.google.com:80/aclk?sa=l&ai=CChPOVvnoU8fMDI_QsQeE4oGwDf664-EF7sq01HqV1MMFCAAQAigDUO3VhpcDYMnGqYvApNgPoAGq3vbiA8gBAaoEKE_QQwekDUoMeW9IQghV4HRuzL_l-7vVjlML559kix6XOcC1c4Tb9xeAB76hiR2QBwGoB6a-Gw&sig=AOD64_3Ulyu0DcDsc1AamOIxq63RF9u4zQ&rct=j&q=&ved=0CCUQ0Qw&adurl=http://www.aruba.com/where-to-stay/hotels-and-resorts%3Ftid%3D122|]
+    , [uri|http://www.example.org/foo#bar|]
+    , [uri|http://www.example.org/foo#|]
+    , [uri|https://www.example.org?listParam%5B%5D=foo,bar|]
+    , [uri|https://www.example.org#only-fragment|]
+    , [uri|https://www.example.org/weird%20path|]
+    , [uri|https://www.example.org?listParam[]=foo,bar|]
+    , [uri|http://www.example.org/.|]
+    , [uri|http:/.|]
+    ]
+
+nonNetworkURIs =
+    [ [uri|https://user:pass:wo%20rd@www.example.org?foo=bar&foo=baz+quux#frag|]
+    ]
+
+-- [relativeRef|verysimple|]
+-- [relativeRef|./this:that/thap/sub?1=2|]
 
 {-
  -instance Arbitrary NU.URIAuth where
@@ -65,7 +108,7 @@ instance Arbitrary UB.Authority where
   shrink = genericShrink
 
 instance Arbitrary UB.Scheme where
-  arbitrary = genericArbitrary
+  arbitrary = UB.Scheme . B8.pack <$> genericArbitrary
   shrink = genericShrink
 
 instance Arbitrary UB.Query where
@@ -80,3 +123,4 @@ instance Arbitrary (UB.URIRef UB.Absolute) where
         uriQuery <- arbitrary
         uriFragment <- arbitrary
         pure UB.URI{..}
+
