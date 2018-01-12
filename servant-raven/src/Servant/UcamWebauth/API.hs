15314148d5c6fe5600a507c13d224f00d3b45955
David Baynard <davidbaynard@gmail.com>
2018-01-12 05:32:57 +0000

    Add new UcamWebAuthCookie endpoint

--- servant-raven/src/Servant/UcamWebauth/API.hs
+++ servant-raven/src/Servant/UcamWebauth/API.hs
@@ -38,14 +38,20 @@ type family Unqueried a = p
 -- | A bifunctional endpoint for authentication, which both delegates and
 -- responds to the Web Login Service (WLS).
 type UcamWebAuthenticate route a
-    = route :> QueryParam "WLS-Response" (SignedAuthResponse 'MaybeValid a) :> Get '[JSON] (UcamWebauthInfo a)
+    = UcamWebAuthToken '[JSON] route (UcamWebauthInfo a) a
 
 type instance Unqueried (UcamWebAuthenticate route a) = route :> Get '[JSON] (UcamWebauthInfo a)
 
 -- | A bifunctional endpoint for authentication, which both delegates and
 -- responds to the Web Login Service (WLS).
 type UcamWebAuthToken typs route token a
-    = route :> QueryParam "WLS-Response" (SignedAuthResponse 'MaybeValid a) :> Get typs token
+    = UcamWebAuthCookie Get typs route token a
 
 type instance Unqueried (UcamWebAuthToken typs route token a) = route :> Get typs token
 
+-- | A bifunctional endpoint for authentication, which both delegates and
+-- responds to the Web Login Service (WLS).
+type UcamWebAuthCookie verb typs route token a
+    = route :> QueryParam "WLS-Response" (SignedAuthResponse 'MaybeValid a) :> verb typs token
+
+type instance Unqueried (UcamWebAuthCookie verb typs route token a) = route :> verb typs token
