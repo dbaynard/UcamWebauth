3fe07103e85914a0fee790a3aa3909638302de70
David Baynard <davidbaynard@gmail.com>
2018-03-22 01:34:55 +0000

    Implement working-ish wlsResponse function

--- ucam-webauth/src/UcamWebauth/WLS.hs
+++ ucam-webauth/src/UcamWebauth/WLS.hs
@@ -14,6 +14,7 @@ This is useful for test suite and more.
 {-# LANGUAGE
     PackageImports
   , OverloadedStrings
+  , PartialTypeSignatures
   #-}
 
 module UcamWebauth.WLS
@@ -21,13 +22,52 @@ module UcamWebauth.WLS
   ) where
 
 import           "aeson"              Data.Aeson (ToJSON)
+import qualified "aeson"              Data.Aeson as A
+import           "ucam-webauth-types" Data.ByteString.B64
+import qualified "bytestring"         Data.ByteString.Builder as B
+import qualified "bytestring"         Data.ByteString.Lazy as BSL
+import           "base"               Data.List (intersperse)
+import           "base"               Data.Maybe
 import           "text"               Data.Text (Text)
-import qualified "text"               Data.Text as T
+import           "text"               Data.Text.Encoding
+import           "microlens"          Lens.Micro
 import           "ucam-webauth-types" UcamWebauth.Data as X
 import           "ucam-webauth-types" UcamWebauth.Data.Internal
-import           "this"               UcamWebauth.Internal
 
 wlsEncode :: ToJSON a => MaybeValidResponse a -> Text
-wlsEncode r = T.intercalate "!"
-  [
-  ]
+wlsEncode r = decodeUtf8 . BSL.toStrict . B.toLazyByteString . mconcat . intersperse "!" $
+    [ response ^. ucamAVer       . to displayWLSVersion
+    , response ^. ucamAStatus    . to (B.stringUtf8 . show . fromEnum)
+    , response ^. ucamAMsg       . mTextEncoded
+    , response ^. ucamAIssue     . encoded (unUcamTime . ucamTime)
+    , response ^. ucamAId        . textEncoded
+    , response ^. ucamAUrl       . textEncoded
+    , response ^. ucamAPrincipal . mTextEncoded
+    , response ^. ucamAPtags     . to (maybe "" $ mconcat . intersperse "," . fmap displayPtag)
+    , response ^. ucamAAuth      . to (maybe "" displayAuthType)
+    , response ^. ucamASso       . to (maybe "" $ mconcat . intersperse "," . fmap displayAuthType)
+    , response ^. ucamALife      . to (maybe "" $ B.stringUtf8 . show . secondsFromTimePeriod)
+    , response ^. ucamAParams    . to (maybe "" $ B.lazyByteString . unUcamB64L . encodeUcamB64L . A.encode)
+    , r        ^. ucamAKid       . to (maybe "" $ B.byteString . unKeyID)
+    , r        ^. ucamASig       . to (maybe "" $ B.byteString . unUcamB64)
+    ]
+  where
+    response = r ^. ucamAResponse
+
+-- orBlank :: Getting r a B.Builder -> Getting r (Maybe a) B.Builder
+-- orBlank g bcrb a = _
+
+textEncoded :: Getting r Text B.Builder
+textEncoded = encoded id
+
+encoded :: ToJSON a => (a -> Text) -> Getting r a B.Builder
+encoded f = to $ encodeUtf8Builder . f
+
+mEncoded :: ToJSON a => (a -> Text) -> Getting r (Maybe a) B.Builder
+mEncoded f = to $ encodeUtf8Builder . fromMaybe "" . fmap f
+
+mTextEncoded :: Getting r (Maybe Text) B.Builder
+mTextEncoded = mEncoded id
+
+-- ucamTimeFormat :: (UTCTime )
+-- ucamTimeFormat = B.stringUtf8 . formatTime defaultTimeLocale "%0Y%m%dT%H%M%SZ"
